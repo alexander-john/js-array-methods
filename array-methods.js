ba253790907d5e609190f4673b5ce342771d6d44
Alexander John <96aljohn@gmail.com>
2024-07-15 20:54:18 -0700

    first commit

---
+++ array-methods.js
@@ -0,0 +1,8 @@
+// Example: Doubling each number in an array
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = numbers.map(num => num * 2);
+console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
+
+// Example: Filtering even numbers from an array
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers); // Output: [2, 4]
